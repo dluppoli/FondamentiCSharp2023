01c09b063116c43dab29396f1fa51deb8d5fac64
dluppoli <davide@davideluppoli.it>
2023-03-24 08:58:56 +0100

    Classi e ref/value types

---
+++ Classi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*DateTime oggi = new DateTime(2023, 03, 23);
+
+            Console.WriteLine(oggi.ToString("ddd dd-MMMM-yyyy") );
+            Console.WriteLine($"Oggi è il {oggi.DayOfYear}° giorno dell'anno");
+            Console.WriteLine(DateTime.IsLeapYear(oggi.Year));*/
+
+            Console.WriteLine("Premi invio per iniziare");
+            Console.ReadLine();
+            DateTime start = DateTime.Now;
+          
+            Console.WriteLine("Premi stop per fermare");
+            Console.ReadLine();
+            DateTime stop = DateTime.Now;
+
+            //Calcolare quanti secondi sono passati
+            TimeSpan intervallo = stop - start;
+            Console.WriteLine($"Sono passati {Math.Round(intervallo.TotalSeconds,0)} secondi");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RefType/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = a;
+            a = 20;
+            Console.WriteLine(a); //20
+            Console.WriteLine(b); //10
+
+
+            int[] c = new int[] { 10, 20 };
+            int[] d = c;
+            c[0] = 30;
+            Console.WriteLine(c[0]); //30
+            Console.WriteLine(d[0]); //30
+
+
+            string s1 = "Hello";
+            string s2 = s1;
+            s1 = "Ciao";
+            Console.WriteLine(s1); //Ciao
+            Console.WriteLine(s2); //Hello
+
+            Console.ReadLine();
+        }
+    }
+}
